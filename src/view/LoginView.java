104d6ee7ac71b0f0aa6e52e654b330dc13cf97ea
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-12-26 10:13:41 -0300

    Alterado a propriedade de criação do form, para quando inicializar, esteja no centro da tela.

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -176,9 +176,9 @@ public class LoginView extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-
-                new LoginView().setVisible(true);
-
+                final LoginView telaLogin = new LoginView();
+                telaLogin.setLocationRelativeTo(null);
+                telaLogin.setVisible(true);
             }
         });
     }
