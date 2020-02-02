6aa5a6e480269b1ec756dc69bcab26afb5ea8027
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2020-02-02 15:47:23 -0300

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,24 @@
+CREATE TABLE nivelAcesso(
+id SERIAL NOT NULL UNIQUE,
+sigla VARCHAR(3) NOT NULL UNIQUE,
+descricao VARCHAR(255),
+PRIMARY KEY(id)
+);
+
+
+    
+CREATE TABLE usuarios(
+id SERIAL NOT NULL UNIQUE,
+nome VARCHAR(255) NOT NULL,
+usuario VARCHAR(255) NOT NULL UNIQUE,
+senha VARCHAR(32) NOT NULL,
+data_nasc DATE,
+nivel INTEGER NOT NULL,
+bloqueado BOOLEAN NOT NULL,
+ativo BOOLEAN NOT NULL,
+tentativas INTEGER CHECK(tentativas <= 3),
+criado TIMESTAMP NOT NULL,
+editado TIMESTAMP,
+PRIMARY KEY(id),
+FOREIGN KEY (nivel) REFERENCES nivelAcesso(id)
+);
